900fd7b8c0f5f8702c0bf0706b561ac41fc94560
lhuizhan <lvhuizhan@lvtekiMacBook-Pro.local>
2015-03-05 00:07:42 -0500

    Huizhan LR_Beam.py ver2.0

--- LR_Beam.py
+++ LR_Beam.py
@@ -10,7 +10,7 @@ optparser.add_option("-t", "--translation-model", dest="tm", default="data/tm", 
 optparser.add_option("-l", "--language-model", dest="lm", default="data/lm", help="File containing ARPA-format language model (default=data/lm)")
 optparser.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxint, type="int", help="Number of sentences to decode (default=no limit)")
 optparser.add_option("-k", "--translations-per-phrase", dest="k", default=5, type="int", help="Limit on number of translations to consider per phrase (default=1)")
-optparser.add_option("-s", "--stack-size", dest="s", default=100, type="int", help="Maximum stack size (default=1)")
+optparser.add_option("-s", "--stack-size", dest="s", default=200, type="int", help="Maximum stack size (default=1)")
 optparser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,  help="Verbose mode (default=off)")
 
 opts = optparser.parse_args()[0]
@@ -18,6 +18,14 @@ opts = optparser.parse_args()[0]
 tm = models.TM(opts.tm, opts.k)
 lm = models.LM(opts.lm)
 french = [tuple(line.strip().split()) for line in open(opts.input).readlines()[:opts.num_sents]]
+
+#parameters
+stepsize = 2.0
+eta = -1.0
+alpha_decrease_rate = 0.9
+epsilon = 0.02
+iteration_limit = 10
+Constraints_limit = 2
 
 # tm should translate unknown words as-is with probability 1
 for word in set(sum(french,())):
@@ -32,8 +40,7 @@ def beam(f, u, Constraints):
   # Hence all hypotheses in stacks[i] represent translations of 
   # the first i words of the input sentence. You should generalize
   # this so that they can represent translations of *any* i words.
-  print 'Constraint = ', Constraints
-  eta = -5.0
+  
   def distortion(end, newstart):
     return eta * abs(end + 1 - newstart)
 
@@ -41,10 +48,9 @@ def beam(f, u, Constraints):
   LR_state = namedtuple("LR_state", "l, m, r, lm_state, bitstring")
   
   initial_y = [0 for i in range(len(f))]
-  initial_bitstring = ''
-  for i in range(len(Constraints)):
-    initial_bitstring.append('0')
-  initial_lrstate = LR_state(0, 0, 0, lm.begin(), initial_bitstring)
+  # initial_bitstring = '0' * len(Constraints)
+  initial_bitstring = 0
+  initial_lrstate = LR_state(0, 0, -1, lm.begin(), initial_bitstring)
   
   initial_hypothesis = hypothesis(0.0, initial_lrstate, None, None, initial_y)
 
@@ -53,7 +59,7 @@ def beam(f, u, Constraints):
   
   for n, stack in enumerate(stacks[:-1]):
     for h in sorted(stack.itervalues(),key=lambda h: -h.logprob)[:opts.s]: # prune
-      for s in xrange(0, len(f)-1):
+      for s in xrange(0, len(f)):
         for t in xrange(s+1, len(f) + 1):
 
           if n + t - s > len(f):
@@ -66,14 +72,7 @@ def beam(f, u, Constraints):
               new_y[tmp] += 1
 
             #Compute the change of u[i] * y[i]            
-            violation_change = 0.0
-            for tmp in range(s, t):
-              violation_change += u[tmp]         
-            # for tmp in xrange(s,t):
-            #   if h.y[tmp] == 0:
-            #     violation_change -= u[tmp]
-            #   else:
-            #     violation_change += u[tmp]
+            violation_change = sum(u[s: t])      
   
             if distortion(h.LR_state.r, s) > 6:
               continue
@@ -85,17 +84,25 @@ def beam(f, u, Constraints):
               lm_state = current_lrstate.lm_state
               
 
-              logprob = h.logprob + phrase.logprob + violation_change + distortion(h.LR_state.r, s)              
+              logprob = h.logprob + phrase.logprob + violation_change + distortion(h.LR_state.r, s)
               
               #Prune using constraint set, or update new constraint set
               new_bitstring = current_lrstate.bitstring
               flag = False
 
+              # for (index, value) in enumerate(Constraints):
+              #   if s<=value and value<t:
+              #     if new_bitstring[index] == '1':
+              #       flag = True
+              #     else:
+              #       new_bitstring[index] == '1'
+
               for (index, value) in enumerate(Constraints):
-                if s<=value and value<=t:
-                  if new_bitstring[index] == '1':
+                if s<=value and value<t:
+                  if (new_bitstring >> index) & 1 == 1:
                     flag = True
-                  new_bitstring[index] == '1'
+                  else:
+                    new_bitstring = new_bitstring | (1 << index)
 
               if flag:
                 continue
@@ -135,14 +142,19 @@ def beam(f, u, Constraints):
                 stacks[new_n][new_lrstate] = new_hypothesis
   winner = max(stacks[-1].itervalues(), key=lambda h: h.logprob)
 
+  final_state = 1 << len(Constraints)
   def isOK(bitstring):
-    return '0' not in bitstring
-
+    return bitstring == final_state -1
+  flag = False
   for h in sorted(stacks[-1].itervalues(),key=lambda h: -h.logprob)[:opts.s]: # prune
     if isOK(h.LR_state.bitstring):
       winner = h
+      flag = True
       break
 
+  # if not flag:
+  #   print "We got a problem!"
+  # print winner
   def extract_english(h): 
     return "" if h.predecessor is None else "%s%s " % (extract_english(h.predecessor), h.phrase.english)
 
@@ -175,7 +187,7 @@ def beam(f, u, Constraints):
   #   for h in sorted(stacks[7].itervalues(),key=lambda h: -h.logprob)[:opts.s]: # prune
   #     print >> test_output, h      
   # test_output.close()
-
+  # sys.exit()
   # print extract_english(winner)
   ans = (winner.logprob, extract_english(winner), winner.y)
   return ans
@@ -185,7 +197,7 @@ def optimize(f, u, Constraints):
   previous = -1e10
   lstar = (-1, -1e10)
   converged = False
-  epsilon = 0.02
+  
 
   def isoptimal(y):
     for yi in y:
@@ -197,14 +209,14 @@ def optimize(f, u, Constraints):
     for (i,yi) in enumerate(y):
       if yi != 1:
         violation_count[i] += 1
-
   now = 0
-  step = 1.0
+  step = stepsize
   while not converged:
-    result = beam(f, u, [])
+    result = beam(f, u, Constraints)
     y = result[2]
-    print 'u = ', u
-    print 'y = ', y
+    # print 'English = ', result[1]
+    # print 'u = ', u
+    # print 'y = ', y
     if isoptimal(result[2]):
       return result
     #is converged?
@@ -218,21 +230,20 @@ def optimize(f, u, Constraints):
       u[i] -= step * (yi - 1)
     #Update stepsize
     if result[0] > previous:
-      step = 0.9 * step
+      step = alpha_decrease_rate * step
     previous = result[0]
     now += 1
-    if (now - lstar[0] > 10):
+    if (now - lstar[0] > iteration_limit):
       break
-    print now, result[0], lstar
-    print '====================================================================='
-
-
+    # print now, result[0], lstar
+    # print '====================================================================='
 
   # print "!"
-  K = 10
+  K = iteration_limit
+  G = Constraints_limit
   # Find the most violated element in y vector
   for iter_count in range(K):
-    result = beam(f, u, [])
+    result = beam(f, u, Constraints)
     y = result[2]
     #update count
     if isoptimal(y):
@@ -244,34 +255,37 @@ def optimize(f, u, Constraints):
       u[i] -= step * (yi - 1)
     #Update stepsize
     if result[0] >previous:
-      step = 0.9 * step
+      step = alpha_decrease_rate * step
     previous = result[0]
 
   new_Constraints = Constraints[:]
-  G = 2
+
   #Add G constraints
   for g in range(G):
-    maxcount = 0
-    maxindex = 0
+    maxcount = -1
+    maxindex = -1
     for (i, vi) in enumerate(violation_count):
       if (i-1) not in new_Constraints and i not in new_Constraints and (i+1) not in new_Constraints:
         if maxcount < vi:
           maxcount = vi
           maxindex = i
-
     new_Constraints.append(maxindex)
     new_Constraints.sort()
-
+  
+  # print new_Constraints
+  # print 'Constraint = ', new_Constraints    
+  
   return optimize(f, u, new_Constraints)
   
 def main():
 
   with open('lr_output','w') as f:
     for fsen in french:
+      # print 'French sentence =', fsen
       u = [0 for i in range(len(fsen))]
       result = optimize(fsen, u, [])
 
-      # step = 1.0
+      # step = stepsize
 
       # for iter_count in range(iterations):
       #   result = beam(fsen, u, [])
@@ -279,7 +293,7 @@ def main():
       #   for (i, yi) in enumerate(y):
       #     u[i] -= step * (yi - 1)
 
-      #   step = 0.9 * step
+      #   step = alpha_decrease_rate * step
         # print result
         # print u
       
@@ -288,7 +302,7 @@ def main():
       #   output = output + ' ' + fsen[i]
       # print output
       print result[1]
-      print >>f, result
+      print >>f, result[1]
       # break
   f.close()
 
